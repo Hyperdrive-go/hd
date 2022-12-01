eac011cab992d36adc2879480099123d6ee142dc
Jason Pierce <jason.pierce.designs@gmail.com>
2022-12-01 11:48:32 -0500

    feat: update tailwind config

--- projects/marketing-site/tailwind.config.js
+++ projects/marketing-site/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        "3xl": "1815px",
+        tall: { raw: "(min-height: 1200px)" },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
