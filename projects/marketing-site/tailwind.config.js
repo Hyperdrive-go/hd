eeb09b3852f2512e57d1c8856fb4a3d948483f20
Jason Pierce <jason.pierce.designs@gmail.com>
2022-12-01 11:56:12 -0500

    feat: update tailwind config

--- projects/marketing-site/tailwind.config.js
+++ projects/marketing-site/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
