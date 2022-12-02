80234e1d7c665e595ad7f4656b0c72b90743e64a
Weston Nelson <westontnelson@outlook.com>
2022-12-02 01:16:34 -0600

    feat(wagmi): added wagmi hooks

---
+++ wagmi.ts
@@ -0,0 +1,35 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+
+const { chains, provider } = configureChains(
+ [chain.mainnet, chain.optimism, chain.arbitrum],
+ [alchemyProvider({ apiKey: 'RbvJ2z-poT2yB-cpZ9xUMaauzUeTQVf5' })],
+)
+
+
+const { webSocketProvider } = configureChains(
+  [chain.mainnet, chain.optimism, chain.arbitrum],
+    [
+      alchemyProvider({
+        apiKey: 'RbvJ2z-poT2yB-cpZ9xUMaauzUeTQVf5',
+        priority: 0,
+      }),
+      publicProvider({priority: 1}),
+   ],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My wagmi + RainbowKit App',
+  chains,
+})
+
+export const client = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+})
+
+export { chains }
