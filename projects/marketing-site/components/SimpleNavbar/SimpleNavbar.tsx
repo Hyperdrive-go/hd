a424a2348cca5bb4d76e3422b9d05782dcb0a5ee
Jason Pierce <jason.pierce.designs@gmail.com>
2022-12-05 14:08:00 -0500

    feat: update navbar used, add hero image, create features list

---
+++ projects/marketing-site/components/SimpleNavbar/SimpleNavbar.tsx
@@ -0,0 +1,123 @@
+import Image from "next/image";
+
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+import logoSVG from "@/images/hd_logo_small.svg";
+import { useState } from "react";
+
+const navigation = [
+  { name: "Vision", href: "/" },
+  { name: "Team", href: "/team" },
+  { name: "Funding", href: "/funding" },
+  { name: "Token", href: "/token" },
+];
+
+const SimpleNavbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <div className="px-6 pt-6 lg:px-8">
+      <div>
+        <nav
+          className="flex h-9 items-center justify-between"
+          aria-label="Global"
+        >
+          <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Hyperdrive</span>
+              <Image
+                className="h-8"
+                src={logoSVG}
+                height={32}
+                width={32}
+                alt="Hyperdrive logo"
+              />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-300"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="font-semibold text-gray-100 hover:text-gray-100"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+            <a
+              href="#"
+              className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-100 shadow-sm ring-1 ring-gray-100/10 hover:ring-gray-100/20"
+            >
+              Soon
+            </a>
+          </div>
+        </nav>
+        <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+            <div className="flex h-9 items-center justify-between">
+              <div className="flex">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                  <Image
+                    className="h-8"
+                    src={logoSVG}
+                    height={32}
+                    width={32}
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div className="flex">
+                <button
+                  type="button"
+                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
+
+export default SimpleNavbar;
--- projects/marketing-site/pages/index.tsx
+++ projects/marketing-site/pages/index.tsx
@@ -1,11 +1,25 @@
-import { DarkNavbar, MainHeroSection } from "@/components/index";
+import { MainHeroSection, Layout } from "@/components/index";
+// import hyperdriveHero from "@/images/large-logo2.png";
+import hyperdriveHeroAlt from "@/images/logo-very-large.png";
+import Image from "next/image";
 
 const LandingPage = () => {
   return (
-    <div>
-      <MainHeroSection />
-      <main>Hello world</main>
-    </div>
+    <Layout>
+      <main>
+        <MainHeroSection />
+        <div className="max-w-7xl mx-auto relative h-[230px] sm:h-[320px] md:h-[420px] lg:h-[600px] xl:h-[920px] my-4 md:my-12 xl:my-24">
+          <Image
+            src={hyperdriveHeroAlt}
+            className="rounded-lg object-contain object-fit"
+            alt=""
+            priority
+            fill
+          />
+        </div>
+        {/* <FeaturesList /> */}
+      </main>
+    </Layout>
   );
 };
 
