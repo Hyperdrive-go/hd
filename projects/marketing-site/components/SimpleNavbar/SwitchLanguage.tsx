54e409dc4c4c81e17858e22337fcd90d4d457088
Anh Nguyen <anh.nt4288@gmail.com>
2022-12-09 15:18:04 +0700

    delete "chain nav bar" add dropdown "user profile"

--- projects/marketing-site/components/SimpleNavbar/SwitchLanguage.tsx
+++ projects/marketing-site/components/SimpleNavbar/SwitchLanguage.tsx
@@ -1,47 +1,64 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { GlobeAltIcon } from '@heroicons/react/20/solid'
-
-import Image from "next/image"
+import { GlobeAltIcon, ChevronRightIcon, CheckIcon } from '@heroicons/react/20/solid'
+import clsx from "clsx";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 
 export default function SwitchLanguage() {
   return (
-    <Menu as="div" className="relative inline-block text-left mr-2">
-      <div>
-        <Menu.Button className="inline-flex w-full justify-center bg-dark px-4 py-2 text-sm font-medium 
-        text-white shadow-sm hover:bg-gray-800">
-            <GlobeAltIcon className="h-6 w-6" aria-hidden="true" />
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
+    <Menu.Item>
+      {({ active }) => (
+      <div
+        className={clsx(
+          active ? "bg-gray-100 w-full cursor-pointer" : "",
+          "block px-4 py-2 text-sm text-gray-700 inline-flex"
+        )}
       >
-        <Menu.Items className="absolute z-10 mt-2 w-32 rounded-md bg-slate-900 shadow-lg ring-1
-         ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            <Menu.Item>
-                <a href="#" className="flex items-center py-2 px-4 text-sm font-medium text-white
-                 shadow-sm ">
-                  English
-                </a>
-            </Menu.Item>
-            <Menu.Item>
-                <a href="#" className="flex items-center py-2 px-4 text-sm font-medium text-white
-                 shadow-sm ">
-                  Thai
-                </a>
-            </Menu.Item>
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
-    );
+        <GlobeAltIcon className="h-6 w-6 mr-2" aria-hidden="true" />
+        Language
+        <Menu as="div" >
+            <Menu.Button className="absolute right-0 mr-4 inline-flex">
+              en <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute left-0 z-50 -mt-3 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      className={clsx(
+                        active ? "bg-gray-100 w-48 cursor-pointer" : "",
+                        "px-4 py-2 text-sm text-gray-700 inline-flex "
+                      )}
+                    >
+                      English <CheckIcon className="h-5 w-5 absolute right-0 mr-4" aria-hidden="true" />
+                    </div>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      className={clsx(
+                        active ? "bg-gray-100 w-48 cursor-pointer" : "",
+                        "px-4 py-2 text-sm text-gray-700"
+                      )}
+                    >
+                      Vietnamese
+                    </div>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+      </div>
+      )}
+      </Menu.Item>
+  );
 }
--- projects/marketing-site/components/SimpleNavbar/SwitchTemplateMode.tsx
+++ projects/marketing-site/components/SimpleNavbar/SwitchTemplateMode.tsx
@@ -1,25 +1,41 @@
-import { Menu } from '@headlessui/react'
-import { SunIcon } from '@heroicons/react/20/solid'
+import { Menu, Switch } from '@headlessui/react'
 import { MoonIcon } from '@heroicons/react/20/solid'
+import clsx from "clsx";
 
 import React, { useState } from 'react';
 
 
 export default function SwitchTemplateMode() {
-  const [dark, setDark] = useState(true);
+  const [enabled, setEnabled] = useState(false)
 
   return (
-    <Menu as="div" className="relative inline-block text-left mr-2">
-      <button className="inline-flex w-full justify-center bg-dark px-4 py-2 text-sm font-medium 
-        text-white shadow-sm hover:bg-gray-800" 
-        onClick={() => setDark(!dark)} >
-        {dark ?
-            <MoonIcon className="h-6 w-6" aria-hidden="true" /> :
-            <SunIcon className="h-6 w-6" aria-hidden="true" /> 
-        }
-      </button>
-
-      
-    </Menu>
+      <Menu.Item>
+        {({ active }) => (
+          <div
+            className={clsx(
+              active ? "bg-gray-100 w-full cursor-pointer" : "",
+              "px-4 py-2 text-sm text-gray-700 inline-flex"
+            )}
+          >
+            <MoonIcon className="h-6 w-6 mr-2" aria-hidden="true" />
+            Dark Mode
+            <div className="absolute right-0 mr-4 inline-flex">
+              <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className={`${
+                  enabled ? 'bg-blue-600' : 'bg-gray-200'
+                }  inline-flex h-6 w-11 items-center rounded-full`}
+              >
+                <span
+                  className={`${
+                    enabled ? 'translate-x-6' : 'translate-x-1'
+                  } inline-block h-4 w-4 transform rounded-full bg-white `}
+                />
+              </Switch>
+            </div>
+          </div>
+        )}
+      </Menu.Item>
     );
 }
