aaf0247637bdc46dd92834ea21624d175e86205b
Jason Pierce <jason.pierce.designs@gmail.com>
2022-12-12 10:15:33 -0500

    chore: fix tests import extended jest commands

--- libs/ui/jest.config.ts
+++ libs/ui/jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
-  setupFiles: ["<rootDir>/src/setupTests.js"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
 
 export default config;
