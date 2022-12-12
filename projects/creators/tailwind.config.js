97f09cd4a7b189745543532a59a0e6fac11f47f0
Jason Pierce <jason.pierce.designs@gmail.com>
2022-12-12 10:21:14 -0500

    chore: update tailwind configs

--- projects/creators/tailwind.config.js
+++ projects/creators/tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [],
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        "3xl": "1815px",
+        tall: { raw: "(min-height: 1200px)" },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
