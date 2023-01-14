1a8e279e773969c89e281e897288cf42ab9257de
Jason Factor <anh.nt4288@gmail.com>
2023-01-14 23:15:41 +0700

    - set BG topnav color to #0B0B0B - update design links for mobile & laptop

--- libs/ui/src/molecules/DarkNavbar/DarkNavbar.tsx
+++ libs/ui/src/molecules/DarkNavbar/DarkNavbar.tsx
@@ -64,7 +64,7 @@ const DarkNavbar = () => {
           fontStack: 'system'
         }
       )} showRecentTransactions={true}>
-        <Disclosure as="nav" className="bg-neutral-900">
+        <Disclosure as="nav" className="bg-topnavBgDark">
           {({ open }) => (
             <>
               <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
@@ -72,14 +72,14 @@ const DarkNavbar = () => {
                   <div className="flex items-center px-2 lg:px-0">
                     <div className="flex-shrink-0">
                       <Image
-                        className="hidden h-8 w-auto lg:block"
+                        className="hidden h-8 w-auto md:block"
                         src={logo}
                         alt="Your Company"
                       />
                     </div>
                   </div>
                   <div className="flex flex-1 justify-center px-2 lg:ml-6">
-                    <div className="w-full max-w-lg lg:max-w-xs">
+                    <div className="w-full max-w-lg sm:max-w-xs">
                       <label htmlFor="search" className="sr-only">
                         Search name, id
                       </label>
@@ -104,19 +104,19 @@ const DarkNavbar = () => {
                   </div>
                   <div className="flex lg:hidden">
                     {/* Mobile menu button */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    {/* <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
                         <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                       ) : (
                         <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                       )}
-                    </Disclosure.Button>
+                    </Disclosure.Button> */}
                   </div>
-                  <div className="hidden lg:ml-4 lg:block">
+                  <div className="lg:ml-4 lg:block">
                     <div className="flex items-center">
-                        <div className="hidden lg:mr-6 lg:ml-6 lg:block">
-                          <div className="flex space-x-4">
+                        <div className="md:mr-2 lg:ml-6 lg:block">
+                          <div className="flex">
                             <a
                               href="#"
                               className="rounded-md bg-gray-900 px-3 py-2 text-sm font-bold text-red-500 hover:bg-gray-700 hover:text-white"
@@ -135,99 +135,15 @@ const DarkNavbar = () => {
                       <ConnectButton
                         label={labelWallet}
                         showBalance={false}
+                        accountStatus={{
+                          smallScreen: 'avatar', 
+                          largeScreen: 'full'
+                        }}
                       />
                     </div>
                   </div>
                 </div>
               </div>
-
-              <Disclosure.Panel className="lg:hidden">
-                <div className="space-y-1 px-2 pt-2 pb-3">
-                  {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                  <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                  >
-                    Dashboard
-                  </Disclosure.Button>
-                  <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                  >
-                    Team
-                  </Disclosure.Button>
-                  <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                  >
-                    Projects
-                  </Disclosure.Button>
-                  <Disclosure.Button
-                    as="a"
-                    href="#"
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                  >
-                    Calendar
-                  </Disclosure.Button>
-                </div>
-                <div className="border-t border-gray-700 pt-4 pb-3">
-                  <div className="flex items-center px-5">
-                    <ConnectButton/>
-                  </div>
-                  <div className="flex items-center pt-4 px-5">
-                    <div className="flex-shrink-0">
-                      <Image
-                        className="h-10 w-10 rounded-full"
-                        height={36}
-                        width={36}
-                        src=""
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium text-white">
-                        Tom Cook
-                      </div>
-                      <div className="text-sm font-medium text-gray-400">
-                        tom@example.com
-                      </div>
-                    </div>
-                    <button
-                      type="button"
-                      className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    <Disclosure.Button
-                      as="a"
-                      href="#"
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                    >
-                      Your Profile
-                    </Disclosure.Button>
-                    <Disclosure.Button
-                      as="a"
-                      href="#"
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                    >
-                      Settings
-                    </Disclosure.Button>
-                    <Disclosure.Button
-                      as="a"
-                      href="#"
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                    >
-                      Sign out
-                    </Disclosure.Button>
-                  </div>
-                </div>
-              </Disclosure.Panel>
             </>
           )}
         </Disclosure>
