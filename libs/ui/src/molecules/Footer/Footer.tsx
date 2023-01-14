336abaef1fd2aedf530a4c3214bab0d17e35e3b5
Jason Factor <anh.nt4288@gmail.com>
2023-01-15 00:25:44 +0700

    adjust footer for mobile & laptop

--- libs/ui/src/molecules/Footer/Footer.tsx
+++ libs/ui/src/molecules/Footer/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           zIndex: -1,
           position: "absolute"
         }}
-        className="w-full md:h-72"
+        className="w-full h-80 md:h-72"
         >
         <Image 
             src={backgroundFooter}
@@ -24,16 +24,16 @@ const Footer = () => {
           />
         </div>
       </div>
-      <div className="mx-auto max-w-7xl grid grid-cols-1 divide-y divide-slate-800 divide-solid md:ml-8">
+      <div className="mx-auto max-w-7xl grid grid-cols-1 md:divide-y divide-slate-800 divide-solid md:ml-12 md:mr-12 lg:ml-16">
         {/* First layer */}
-        <div className="relative flex h-44 pt-12">
-            <div className="items-center px-2 lg:px-0">
+        <div className="relative md:flex h-44 pt-12">
+            <div className="items-center px-4 md:px-2 lg:px-0">
               <Image
                 className="hidden h-8 w-auto lg:block"
                 src={logo}
                 alt="Your Company"
               />
-              <div className="pt-4">
+              <div className="md:pt-4">
                 <div className="rounded-md text-sm font-medium text-white">
                     Discover, collect, and sell digital items on the
                 </div>
@@ -42,10 +42,10 @@ const Footer = () => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-1 justify-center lg:justify-end">
-              <div className="lg:mr-12">
-                <h3 className="text-white text-xl font-medium">Join the community</h3>
-                <div className="flex pt-6 space-x-4">
+            <div className="pt-6 px-4 md:px-0 md:pt-0 md:flex flex-1 justify-center md:justify-end">
+              <div>
+                <h3 className="text-white md:text-xl font-medium">Join the community</h3>
+                <div className="flex pt-4 md:pt-6 space-x-4">
                   <button
                       type="button"
                       className="flex-shrink-0 px-4 py-4 rounded-lg bg-blue-900"
@@ -69,50 +69,51 @@ const Footer = () => {
             </div>
         </div>
         {/* Second layer */}
-        <div className="flex h-24 pt-4">
+        <div className="md:flex px-4 h-28 pt-10 md:pt-4 md:px-0">
+            <hr className="md:hidden pt-1 bg-slate-800 border-0"/>
             <div className="items-center">
-              <div className="flex space-x-4">
-                <div className="rounded-md text-sm font-medium text-white py-2">
+              <div className="md:flex space-x-4">
+                <div className="rounded-md text-sm font-medium text-white py-2 pt-4 md:pt-0">
                   @2022 HyperDrive. All rights reserved.
                 </div>
               </div>
             </div>
-            <div className="flex flex-1 justify-center px-2 md:px-8 lg:ml-6 lg:justify-end">
+            <div className="md:flex flex-1 justify-end">
               <div>
-                <div className="flex space-x-4">
+                <div className="md:flex space-x-2 md:space-x-8 ">
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
+                    className="rounded-md py-2 text-sm font-medium text-white"
                   >
                     Help
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
+                    className="rounded-md py-2 text-sm font-medium text-white"
                   >
                     Status
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
+                    className="rounded-md py-2 text-sm font-medium text-white"
                   >
                     API
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
+                    className="rounded-md py-2 text-sm font-medium text-white"
                   >
                     Brand
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
+                    className="rounded-md py-2 text-sm font-medium text-white"
                   >
                     Terms
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
+                    className="rounded-md py-2 text-sm font-medium text-white"
                   >
                     Privacy
                   </a>
