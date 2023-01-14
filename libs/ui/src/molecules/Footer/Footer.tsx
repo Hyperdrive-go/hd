95e6bace58149f75a6523e7940faed26bcb1e015
Jason Factor <anh.nt4288@gmail.com>
2023-01-14 20:45:03 +0700

    - change text to "largest Layer 2 NFT marketplace" - add image background cover for footer - change button social color - adjust footer for md & lg screen

--- libs/ui/src/molecules/Footer/Footer.tsx
+++ libs/ui/src/molecules/Footer/Footer.tsx
@@ -3,11 +3,28 @@ import Image from "next/image";
 import { FaTwitter, FaDiscord, FaTelegram } from "react-icons/fa";
 
 const logo = require('../../assets/images/logo.png');
+const backgroundFooter = require('../../assets/images/backgroundFooter.png');
 
 const Footer = () => {
   return (
-    <footer style={{ background: `rgb(242,55,62) linear-gradient(179deg, rgba(242,55,62,1) 0%, rgba(33,54,174,1) 99%, rgba(34,54,175,1) 100%)` }}>
-      <div className="mx-auto max-w-7xl">
+    <footer>
+      {/* Background footer */}
+      <div className="mx-auto">
+        <div style={{
+          zIndex: -1,
+          position: "absolute"
+        }}
+        className="w-full md:h-72"
+        >
+        <Image 
+            src={backgroundFooter}
+            alt="background"
+            className="cover"
+            fill
+          />
+        </div>
+      </div>
+      <div className="mx-auto max-w-7xl grid grid-cols-1 divide-y divide-slate-800 divide-solid md:ml-8">
         {/* First layer */}
         <div className="relative flex h-44 pt-12">
             <div className="items-center px-2 lg:px-0">
@@ -21,47 +38,46 @@ const Footer = () => {
                     Discover, collect, and sell digital items on the
                 </div>
                 <div className="rounded-md text-sm font-medium text-white">
-                  largest NFT marketplace on Optimism
+                  largest Layer 2 NFT marketplace
                 </div>
               </div>
             </div>
             <div className="flex flex-1 justify-center lg:justify-end">
-              <div className="lg:mr-20">
+              <div className="lg:mr-12">
                 <h3 className="text-white text-xl font-medium">Join the community</h3>
-                <div className="flex pt-6 space-x-6">
+                <div className="flex pt-6 space-x-4">
                   <button
                       type="button"
-                      className="flex-shrink-0 px-2 py-2 rounded-lg bg-sky-900"
+                      className="flex-shrink-0 px-4 py-4 rounded-lg bg-blue-900"
                     >
-                  <FaTwitter className="h-6 w-6 text-white"/>
+                  <FaTwitter className="h-5 w-5 text-white"/>
                   </button>
                   <button
                       type="button"
-                      className="flex-shrink-0 px-2 py-2 rounded-lg bg-sky-900"
+                      className="flex-shrink-0 px-4 py-4 rounded-lg bg-blue-900"
                     >
-                  <FaDiscord className="h-6 w-6 text-white"/>
+                  <FaDiscord className="h-5 w-5 text-white"/>
                   </button>
                   <button
                       type="button"
-                      className="flex-shrink-0 px-2 py-2 rounded-lg bg-sky-900"
+                      className="flex-shrink-0 px-4 py-4 rounded-lg bg-blue-900"
                     >
-                  <FaTelegram className="h-6 w-6 text-white"/>
+                  <FaTelegram className="h-5 w-5 text-white"/>
                   </button>
                 </div>
               </div>
             </div>
-          </div>
-        <hr/>
+        </div>
         {/* Second layer */}
         <div className="flex h-24 pt-4">
             <div className="items-center">
               <div className="flex space-x-4">
-                <div className="rounded-md text-sm font-medium text-white px-3 py-2 ">
+                <div className="rounded-md text-sm font-medium text-white py-2">
                   @2022 HyperDrive. All rights reserved.
                 </div>
               </div>
             </div>
-            <div className="flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end">
+            <div className="flex flex-1 justify-center px-2 md:px-8 lg:ml-6 lg:justify-end">
               <div>
                 <div className="flex space-x-4">
                   <a
