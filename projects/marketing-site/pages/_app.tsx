2562d4a731606236af01a1ce245e7ddf131c0946
Deren Somsanith <derensomsanith@gmail.com>
2023-02-08 15:59:09 -0600

    feat: add GTM

--- projects/marketing-site/pages/_app.tsx
+++ projects/marketing-site/pages/_app.tsx
@@ -1,14 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from 'next-themes'
 import { appWithTranslation } from 'next-i18next'
+import Script from 'next/script'
 
 function App({ Component, pageProps }: AppProps) {
+  const gtmId = process.env.GA_MEASUREMENT_ID
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtmId}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
-  );
+  )
 }
 
-export default appWithTranslation(App);
+export default appWithTranslation(App)
