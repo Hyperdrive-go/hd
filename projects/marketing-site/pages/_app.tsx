82cb76060495681ec5d9f0821bd9487305343593
Deren Somsanith <derensomsanith@gmail.com>
2023-02-15 19:11:35 -0600

    chore: organize imports

--- projects/marketing-site/pages/_app.tsx
+++ projects/marketing-site/pages/_app.tsx
@@ -3,10 +3,9 @@ import '../styles/globals.css'
 import { appWithTranslation } from 'next-i18next'
 import { ThemeProvider } from 'next-themes'
 import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 
 import type { AppProps } from 'next/app'
-
-import TagManager from 'react-gtm-module'
 
 function App({ Component, pageProps }: AppProps) {
   const gtmId = process.env.GA_MEASUREMENT_ID
