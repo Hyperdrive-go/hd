6f56ba0e882107a5602f7556c21c0072077b5ea3
Deren Somsanith <derensomsanith@gmail.com>
2023-02-15 19:10:02 -0600

    feat: add react-gtm-module

--- projects/marketing-site/pages/_app.tsx
+++ projects/marketing-site/pages/_app.tsx
@@ -1,28 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
+
 import { appWithTranslation } from 'next-i18next'
-import Script from 'next/script'
+import { ThemeProvider } from 'next-themes'
+import { useEffect } from 'react'
+
+import type { AppProps } from 'next/app'
+
+import TagManager from 'react-gtm-module'
 
 function App({ Component, pageProps }: AppProps) {
   const gtmId = process.env.GA_MEASUREMENT_ID
 
+  useEffect(() => {
+    if (gtmId) TagManager.initialize({ gtmId })
+  }, [gtmId])
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      {/* Global site tag (gtag.js) - Google Analytics */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${gtmId}');
-        `}
-      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   )
