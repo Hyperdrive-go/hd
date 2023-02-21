ec9c87cadf916451253006ff69757c2a5fdefe02
Jason Factor <anh.nt4288@gmail.com>
2023-02-21 20:41:46 +0700

    add libs/api

---
+++ libs/api/src/common/config.ts
@@ -0,0 +1,12 @@
+export const siteConfig =
+{
+    CHAIN_NAME: "Optimism Goerli",
+    CHAIN_ID: "0x1a4",
+    NETWORK: "opt-goerli",
+    RPC_URL: "https://goerli.optimism.io",
+    L1_BLOCK_EXPLORER_URL: "https://goerli.etherscan.io",
+    L2_BLOCK_EXPLORER_URL: "https://goerli-optimism.etherscan.io",
+    LOGO_BADGE: "Goerli",
+    BACKEND_URL: "https://hd-backend-api.herokuapp.com",
+    BACKEND_TOKEN: "cc919b6a43b0921fc30fd3a6dbfdd9cdd636ffa9",
+}
---
+++ libs/api/src/common/utils.ts
@@ -0,0 +1,11 @@
+// This list should match backend ActivityType model
+export const activityEventRegistry = {
+    Mint: "MI",
+    Sale: "SA",
+    Transfer: "TR",
+    Offer: "OF",
+    List: "LI",
+    Burn: "BU",
+    Airdrop: "AD",
+    Bridge: "BR",
+  };
