8b52dfb9d5e6e4906daeb16d964966add9a744a2
Jason Factor <anh.nt4288@gmail.com>
2023-03-04 23:18:26 +0700

    - add discord & twitter link - remove telegram - add mx-auto in footer

--- libs/ui/src/molecules/Footer/Footer.tsx
+++ libs/ui/src/molecules/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const backgroundFooter = require('../../assets/images/backgroundFooter.png');
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="mx-auto">
       {/* Background footer */}
       <div className="mx-auto">
         <div style={{
@@ -45,25 +45,19 @@ const Footer = () => {
             <div className="pt-6 px-4 md:px-0 md:pt-0 md:flex flex-1 justify-center md:justify-end">
               <div>
                 <h3 className="text-white md:text-xl font-medium">Join the community</h3>
-                <div className="flex pt-4 md:pt-6 space-x-4">
-                  <button
-                      type="button"
-                      className="flex-shrink-0 px-4 py-4 rounded-lg bg-blue-900"
+                <div className="flex pt-4 md:pt-6 space-x-4 justify-start md:justify-end">
+                  <a
+                      href={process.env.NEXT_PUBLIC_TWITTER_URL} target="_blank"
+                      className="px-4 py-4 rounded-lg bg-blue-900"
                     >
-                  <FaTwitter className="h-5 w-5 text-white"/>
-                  </button>
-                  <button
-                      type="button"
-                      className="flex-shrink-0 px-4 py-4 rounded-lg bg-blue-900"
+                    <FaTwitter className="h-5 w-5 text-white"/>
+                  </a>
+                  <a
+                      href={process.env.NEXT_PUBLIC_DISCORD_URL} target="_blank"
+                      className="px-4 py-4 rounded-lg bg-blue-900"
                     >
-                  <FaDiscord className="h-5 w-5 text-white"/>
-                  </button>
-                  <button
-                      type="button"
-                      className="flex-shrink-0 px-4 py-4 rounded-lg bg-blue-900"
-                    >
-                  <FaTelegram className="h-5 w-5 text-white"/>
-                  </button>
+                      <FaDiscord className="h-5 w-5 text-white"/>
+                  </a>
                 </div>
               </div>
             </div>
@@ -74,7 +68,7 @@ const Footer = () => {
             <div className="items-center">
               <div className="md:flex space-x-4">
                 <div className="rounded-md text-sm font-medium text-white py-2 lg:pt-4">
-                  @2022 HyperDrive. All rights reserved.
+                  @2023 HyperDrive. All rights reserved.
                 </div>
               </div>
             </div>
